44d53129970e7bad93fe15c77dc5534db56da70c
avan31017 <avan31017@gmail.com>
2024-06-21 09:31:33 +0900

    Create Project

---
+++ SearchArticle/SearchArticleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchArticleApp.swift
+//  SearchArticle
+//
+//  Created by 조성빈 on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchArticleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
